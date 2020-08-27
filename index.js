3ca45170c431c48d5ba6af43797e65627f166d1d
Bipin Babu <bipinbabu@live.com>
2020-08-28 02:38:37 +0530

    Generalized the function for taking custom skippable i/p

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-let input = 3;
+let input = 5; //No of steps in the stair
+let X = [1,3,5];  //Array holding no.of skippable steps in the stair
+
+//Assuming the array is sorted
+let top = X[X.length - 1];
 
 //function that returns no.of unique ways to climb steps
 function getCount(N){
@@ -8,7 +12,19 @@ function getCount(N){
     if(N == 2){
         return N;
     }
-    return getCount(N - 1)+ getCount(N - 2);
+    let j = 0;
+    let i = X[j];
+
+    let sum = 0;
+    while(i <= top && j < X.length){
+        let diff = N - i
+        sum += getCount(diff);
+        j ++;
+        i = X[j];
+    }
+
+    return sum;
+    // return getCount(N - 1)+ getCount(N - 2);
 }
 
 console.log(getCount(input));
